ea59e11fb2142bedf21d845d264e91b7a48ca433
Navy <navych@126.com>
2024-06-04 21:59:58 +0800

    add Tabix

--- pattools/io.py
+++ pattools/io.py
@@ -1,5 +1,6 @@
 import sys
 import pysam
+from typing import List
 
 
 class Output:
@@ -52,3 +53,68 @@ class Output:
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.close()
         return False
+
+
+class Tabix:
+    def __init__(self, filename: str, region: str | List[str] = None):
+        self._filename = filename
+        self._filename_csi = filename + '.csi'
+        self._regions = region
+        self._regions_pointer = -1
+        self._iterator = None
+        self._tabixfile = pysam.TabixFile(self._filename, index=self._filename_csi)
+        self._is_multi_region = not (self._regions is None or isinstance(self._regions, str))
+        if self._is_multi_region:
+            self._regions_pointer = 0
+        else:
+            self._iterator = self._tabixfile.fetch(region=self._regions)
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._is_multi_region:
+            if self._regions_pointer < len(self._regions):
+                if self._iterator is None:
+                    _region = self._regions[self._regions_pointer]
+                    self._iterator = self._tabixfile.fetch(region=_region)
+                try:
+                    return self._parse_line(next(self._iterator))
+                except StopIteration:
+                    self._regions_pointer += 1
+                    self._iterator = None
+                    return self.__next__()
+            else:
+                raise StopIteration
+        else:
+            return self._parse_line(next(self._iterator))
+
+    def _parse_line(self, line: str):
+        return line
+
+    def readline(self):
+        try:
+            return self.__next__()
+        except StopIteration:
+            return None
+
+
+class CpGTabix(Tabix):
+    def __init__(self, filename, region=None):
+        super(CpGTabix, self).__init__(filename, region)
+
+    def _parse_line(self, line):
+        row = line.split('\t')
+        return row[0], int(row[2])
+
+
+class MotifTabix(Tabix):
+    def __init__(self, filename, region=None):
+        super(MotifTabix, self).__init__(filename, region)
+
+    def _parse_line(self, line: str):
+        row = line.strip().split('\t')
+        chrom = row[0]
+        cpg_idx = int(row[1])
+        motif_count_arr = [int(x) for x in row[2:]]
+        return chrom, cpg_idx, motif_count_arr
