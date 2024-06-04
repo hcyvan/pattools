ee1f107f1597ef03e7b1f8ef9ca30e9b0df382c8
style608113@163.com <style608113@163.com>
2024-06-04 16:59:53 +0800

    new file:   pattools/matrixgenerate.py

--- pattools/__init__.py
+++ pattools/__init__.py
@@ -3,7 +3,7 @@ from pattools.deconv import deconvolution_sun, deconvolution_moss, deconvolution
 from pattools.entropy import extract_entropy
 from pattools.beta import extract_beta
 from pattools.format import pat2motif
-
+from pattools.matrixgenerate import matrix_generate
 
 def main():
     parser = argparse.ArgumentParser(prog='pattools',
@@ -70,6 +70,17 @@ def main():
     parser_pat2motif.add_argument('-w', '--window', type=int, default='4',
                                   help='Define the length of motif, such as ''3:CCT; 4: CCTT; 5:CCTTT'' ')
     # ======================================================================
+    parser_matrix_generate = subparsers.add_parser('matrix_generate',
+                                             help='This command is used to generate matrix for entropy and beta')
+    parser_matrix_generate.add_argument('-i', '--input', required=True, help='The input file')
+    parser_matrix_generate.add_argument('-o', '--out', default=None, help='The output file')
+    parser_matrix_generate.add_argument('-c', '--coordinate', help='If set, files are not '
+                                                                      'compressed with bgzip')
+    parser_matrix_generate.add_argument('-d', '--depth', type=int, help='If set, files are not '
+                                                                      'compressed with bgzip')
+    parser_matrix_generate.add_argument('-e', '--exclude_mode', help='If set, files are not '
+                                                                      'compressed with bgzip')
+    # ======================================================================    
     args = parser.parse_args()
     if args.sub == 'deconv':
         if args.method == 'sun':
@@ -89,3 +100,5 @@ def main():
         pass
     if args.sub == 'pat2motif':
         pat2motif(args.input, args.out, args.window, not args.text)
+    if args.sub == 'matrix_generate':
+        matrix_generate(args.input, args.coordinate, args.depth, args.exclude_mode, args.out)        
