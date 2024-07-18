5c499025f4b5bcba212b7cdb362b2af61e3de85c
Navy <navych@126.com>
2024-07-18 11:12:42 +0800

    Added support for inputting intervals via motif files

--- pattools/vector/support.py
+++ pattools/vector/support.py
@@ -5,9 +5,9 @@ from pattools.vector.utils import parse_file_list
 def extract_motif_from_region(file_list, region, outfile=None):
     input_files, groups, samples = parse_file_list(file_list)
     with Output(filename=outfile, bgzip=False) as of:
-        of.write(f'#group\tsample\tmotif\n')
+        of.write(f'#chrom\tCpG_index\tgroup\tsample\tmotif\n')
         for i, motif_file in enumerate(input_files):
             with MotifTabix(motif_file, region) as tabix:
-                line = tabix.readline()
-                chrom, cpg_idx, motif_count = line
-                of.write(f'{groups[i]}\t{samples[i]}\t{"|".join([str(x) for x in motif_count])}\n')
+                for line in tabix:
+                    chrom, cpg_idx, motif_count = line
+                    of.write(f'{chrom}\t{cpg_idx}\t{groups[i]}\t{samples[i]}\t{"|".join([str(x) for x in motif_count])}\n')
